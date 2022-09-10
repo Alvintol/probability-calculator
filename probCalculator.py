8ab439f4738c811c12b031e03ddc72deb7da3f4e
Alvin <alvin.tol@hotmail.com>
2022-09-10 12:13:02 -0600

    adds contents and loop to fill contents list for every number of colored ball

--- probCalculator.py
+++ probCalculator.py
@@ -1,3 +1,14 @@
 class Hat:
-  def __init__(self, contents):
-    self.contents = contents
+    def __init__(self, **balls):
+        self.contents = []
+        for color, number in balls.items():
+            for ball in range(number):
+                self.contents.append(color)
+
+        print('BALLS:', balls.items())
+            # print('CONTENTS:', self.contents)
+
+
+Hat(yellow=3, blue=2, green=6)
+Hat(red=5, orange=4)
+Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
