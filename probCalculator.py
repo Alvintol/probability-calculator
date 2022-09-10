169b5fbf6d6e21d3537d8b1e01826fadd1d1a415
Alvin <alvin.tol@hotmail.com>
2022-09-10 13:29:39 -0600

    corrects hat.draw method

--- probCalculator.py
+++ probCalculator.py
@@ -19,22 +19,28 @@ class Hat:
         if len(contentsCopy) == 0:
           contentsCopy = copy(self.contents)
         
-        index = randint(1, numToDraw)   
+        index = randint(1, numToDraw)  
+        print('INDEX:', index) 
         ballPulled = contentsCopy[index]
         pulledFromHat.append(ballPulled)
-        contentsCopy.remove(contentsCopy[index])
+        contentsCopy.pop(index)
+        numToDraw -= 1
       return pulledFromHat
     
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-  expectedOutcome = 0
-  experimentsRan = 0
+# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+#   expectedOutcome = 0
+#   experimentsRan = 0
   
+#   while experimentsRan <= num_experiments:
+#     if ()
+#     hat.draw(num_balls_drawn)
   
-  
-  return expectedOutcome / num_experiments
+#   return expectedOutcome / num_experiments
 
     
 
-Hat(yellow=3, blue=2, green=6)
-Hat(red=5, orange=4)
-Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+hat1 = Hat(yellow=3, blue=2, green=6)
+hat2 = Hat(red=5, orange=4)
+hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+
+print(hat1.draw(5))
