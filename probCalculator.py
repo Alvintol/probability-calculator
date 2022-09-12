708ea9a1d6542832bddfc711dea0576b6f9c9901
Alvin <alvin.tol@hotmail.com>
2022-09-12 16:57:15 -0600

    reduces allsdrawn list to individual all totals

--- probCalculator.py
+++ probCalculator.py
@@ -1,7 +1,8 @@
-import random 
+import random
 import copy
 from random import randint
 from copy import copy
+
 
 class Hat:
     def __init__(self, **balls):
@@ -11,34 +12,47 @@ class Hat:
                 self.contents.append(color)
 
     def draw(self, num):
-      numToDraw = num
-      contentsCopy = copy(self.contents)
-      pulledFromHat = []
-      
-      while numToDraw > 0:
-        if len(contentsCopy) == 0:
-          contentsCopy = copy(self.contents)
-        
-        index = randint(0, len(contentsCopy) - 1)  
-        ballPulled = contentsCopy[index]
-        pulledFromHat.append(ballPulled)
-        contentsCopy.pop(index)
-        numToDraw -= 1
-        
-      return pulledFromHat
-    
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-  expectedOutcome = 0
-  experimentsRan = 0
-  
-  while experimentsRan <= num_experiments:
-    # if hat.draw(num_balls_drawn):
-      
-    experimentsRan += 1
-  
-  return expectedOutcome / num_experiments
+        numToDraw = num
+        contentsCopy = copy(self.contents)
+        pulledFromHat = []
 
-    
+        while numToDraw > 0:
+            if len(contentsCopy) == 0:
+                contentsCopy = copy(self.contents)
+
+            index = randint(0, len(contentsCopy) - 1)
+            ballPulled = contentsCopy[index]
+            pulledFromHat.append(ballPulled)
+            contentsCopy.pop(index)
+            numToDraw -= 1
+
+        return pulledFromHat
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    expectedOutcome = 0
+    experimentsRan = 0
+
+    while experimentsRan <= num_experiments:
+        ballsDrawn = hat.draw(num_balls_drawn)
+        drawnTotal = {}
+        print('DRAWNTOTAL1:', drawnTotal)
+
+        for ball in ballsDrawn:
+            print('BALL:', ball)
+            if ball in drawnTotal:
+                drawnTotal[ball] += 1
+            else:
+                drawnTotal[ball] = 1
+                
+        print('DRAWNTOTAL2:', drawnTotal)
+
+        # for expectedBall, value in expected_balls.items():
+
+        experimentsRan += 1
+
+    return expectedOutcome / num_experiments
+
 
 hat1 = Hat(yellow=3, blue=2, green=6)
 hat2 = Hat(red=5, orange=4)
